ab385f976cbb01dc37f76994c54f9f80381e73f7
ulyssesmm <57453282+ulyssesmm@users.noreply.github.com>
2023-05-08 22:14:52 -0600

    Create Lexer.hpp

---
+++ include/Lexer.hpp
@@ -0,0 +1,26 @@
+#ifndef __LEXER_HPP__
+#define __LEXER_HPP__
+
+#if !defined(yyFlexLexerOnce)
+#include <FlexLexer.h>
+#endif
+
+#include <string>
+using namespace std;
+
+class Lexer : public yyFlexLexer
+{
+public:
+    Lexer(std::istream *in) : yyFlexLexer(in){    
+    }
+    ~Lexer() = default;
+    using FlexLexer::yylex;
+    virtual int yylex();
+
+    int getLine(); 
+    
+private:
+    int line=1;
+};
+
+#endif // !__LEXER_HPP__
